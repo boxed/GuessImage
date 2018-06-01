13da586d1d4a988577d4b15a157a1088489785e3
Anders Hovmöller <boxed@killingar.net>
2018-06-01 06:40:39 +0200

    Initial Commit

---
+++ GuessImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessImage
+//
+//  Created by Anders Hovmöller on 2018-06-01.
+//  Copyright © 2018 Anders Hovmöller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
